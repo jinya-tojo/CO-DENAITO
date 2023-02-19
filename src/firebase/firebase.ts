f1fa597252108ef3668b2b07b1c81f70b588b12d
jinyatojo <jinya.tojo1220@gmail.com>
2023-02-19 18:33:16 +0900

    firebaseの初期設定とログイン、ユーザー登録機能を実装

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,44 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+}
+
+export const app = initializeApp(firebaseConfig)
+
+const auth = getAuth(app)
+
+export const signUp = (email: string, password: string) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      if (errorCode === 'auth/email-already-in-use') {
+        alert('すでに登録済みのユーザーです。')
+      }
+    })
+}
+
+export const login = (email: string, password: string) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user
+      console.log(user)
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+    })
+}
