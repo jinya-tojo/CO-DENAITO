fe7548d23dc68b46b4dbc87d77c37903d42fc2bf
jinyatojo <jinya.tojo1220@gmail.com>
2023-02-21 11:55:59 +0900

    投稿カードコンポーネントの作成

---
+++ src/components/Postcard/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Image from 'next/image'
+import imageSrc from '../photos/sampleClothe.jpeg'
+import iconSrc from '../photos/addIcon3x.png'
+import {
+  styles,
+  getSizeStyle,
+  getImageSizeStyle,
+  itemStyles,
+  categoryStyles,
+  iconStyles,
+  iconWithWantStyles,
+  wantStyles,
+} from './styles'
+
+type PostcardProps = {
+  width: string
+  height: string
+  bland: string
+  item: string
+  want: string
+  category: string
+}
+
+export const Postcard: React.FC<PostcardProps> = ({
+  width,
+  height,
+  bland,
+  item,
+  want,
+  category,
+}) => {
+  return (
+    <>
+      <div css={getSizeStyle(width)}>
+        <div css={getImageSizeStyle(height)}>
+          <Image src={imageSrc} fill alt="投稿画像" />
+        </div>
+        <div>
+          <p css={styles.bland}>{bland}</p>
+          <p css={itemStyles.item}>{item}</p>
+          <div css={iconWithWantStyles.iconWithWant}>
+            <div css={iconStyles.icon}>
+              <Image src={iconSrc} fill alt="addアイコン" />
+            </div>
+            <p css={wantStyles.want}>{want}</p>
+          </div>
+
+          <p css={categoryStyles.category}>{category}</p>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/components/Postcard/styles.ts
@@ -0,0 +1,90 @@
+import { css } from '@emotion/react'
+import makeStyles from 'src/styles/makeStyles'
+
+export const getSizeStyle = (width: string) => {
+  switch (width) {
+    case 'three':
+      return css`
+        width: 240px;
+        height: 380px;
+      `
+    case 'four':
+      return css`
+        width: 180px;
+        height: 284px;
+      `
+  }
+}
+
+export const getImageSizeStyle = (height: string) => {
+  switch (height) {
+    case 'three':
+      return css`
+        width: 240px;
+        height: 280px;
+        position: relative;
+      `
+    case 'four':
+      return css`
+        width: 180px;
+        height: 210px;
+        position: relative;
+      `
+  }
+}
+
+export const imageStyles = makeStyles({
+  photo: () => css`
+    width: 100%;
+    height: 40px;
+  `,
+})
+
+export const styles = makeStyles({
+  bland: () => css`
+    color: #999;
+    font-size: 10px;
+  `,
+})
+
+export const itemStyles = makeStyles({
+  item: () => css`
+    font-size: 16px;
+    color: #333;
+  `,
+})
+
+export const iconWithWantStyles = makeStyles({
+  iconWithWant: () => css`
+    display: flex;
+    padding-bottom: 5px;
+  `,
+})
+
+export const iconStyles = makeStyles({
+  icon: () => css`
+    position: relative;
+    width: 18px;
+    height: 18px;
+  `,
+})
+
+export const wantStyles = makeStyles({
+  want: () => css`
+    font-size: 12px;
+    color: #333;
+    line-height: 18px;
+    padding-left: 5px;
+  `,
+})
+
+export const categoryStyles = makeStyles({
+  category: () => css`
+    color: white;
+    border-radius: 2px;
+    font-size: 12px;
+    padding: 4px 8px;
+    background-color: #c27021;
+    display: inline-block;
+  `,
+})
