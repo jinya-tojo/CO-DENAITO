7e4182ea627f45f2171fb8a3de1dd9e88540fed1
jinyatojo <jinya.tojo1220@gmail.com>
2023-03-05 21:57:35 +0900

    feature: 色々作成

--- src/pages/suggest/index.tsx
+++ src/pages/suggest/index.tsx
@@ -14,6 +14,7 @@ const Suggest: React.FC = () => {
   const [item, setItem] = useState('')
   const [bland, setBland] = useState('')
   const [text, setText] = useState('')
+  const [category, setCategory] = useState('')
   const router = useRouter()
 
   const onClickButton = () => {
@@ -42,7 +43,10 @@ const Suggest: React.FC = () => {
             label="ブランド名"
             onChange={(v: string) => setItem(v)}
           />
-          <SelectCategory />
+          <SelectCategory
+            value={category}
+            onChange={(v: string) => setCategory(v)}
+          />
           <TextInput
             tag="yes"
             width="long"
